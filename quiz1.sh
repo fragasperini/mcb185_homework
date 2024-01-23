9ad56bb77cd75c88d91f18485e20aec79cbd8765
fragasperini <francescacorinnagasperini@gmail.com>
2024-01-23 09:35:58 -0800

    quiz1.sh

--- quiz1.sh
+++ quiz1.sh
@@ -1,9 +1,13 @@
-Name: Francesca Corinna Gasperini
-Linux user name: francescacorinnagasperini
+# Name: Francesca Corinna Gasperini
+# Linux user name: francescacorinnagasperini
 
+# Francesca C. Gasperini, Amina Muhic
+
+#spellingbee 1
 gunzip -c ~/Code/MCB185/data/dictionary.gz |grep "a" | grep -E "^[mcftoua]{4,}$"
 gunzip -c ~/Code/MCB185/data/dictionary.gz |grep "a" | grep -E "^[mcftoua]{4,}$" | wc -l 
 
+#spellingbee 2
 gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -v "[^trailnb] | grep -E {.4}
-gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -v "[^trailnb] | grep -E {.4} | wc -l
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -v [^trailnb] | grep -E {.4} | wc -l
 
