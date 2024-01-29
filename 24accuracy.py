ed51fab01ecb5bed99ba11e97f006b04d8fcee26
fragasperini <francescacorinnagasperini@gmail.com>
2024-01-28 22:48:29 -0800

    24accuracy.py

---
+++ 24accuracy.py
@@ -0,0 +1,30 @@
+import math 
+
+def accuracy(fn, fp, tn, tp):  
+# TP is true positives, FP is false positives, TN is true negatives, FN is false negatives
+	total = (fn + fp + tn + tp)
+	assert(fn >= 0)
+	assert(fp >= 0)
+	assert(tn >= 0)
+	assert(tp >= 0)
+	ac = (tn + tp) / total      # Accuracy
+	p = tp / (fp + tp)          # Precision
+	r = tp / (fn + tp)          # Recall
+	f1 = 2*p*r / (p + r)        # F1-score
+	
+	return ac, f1
+	
+ac, f1 = accuracy(5, 2, 10, 15)
+print(ac, f1)
+
+ac, f1 = accuracy(5, 9, 78, 55)
+print(ac, f1)
+
+ac, f1 = accuracy(35, 0, 53, 70)
+print(ac, f1)
+
+ac, f1 = accuracy(0, 0, 24, 31)
+print(ac, f1)
+
+ac, f1 = accuracy(-3, 8, 64, 30)
+print(ac, f1)
