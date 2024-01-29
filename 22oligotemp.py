fb50bcd2827e119203521550bfa8ccfe480c15a4
fragasperini <francescacorinnagasperini@gmail.com>
2024-01-29 00:49:10 -0800

    22oligotemp.py

--- 22oligotemp.py
+++ 22oligotemp.py
@@ -1,18 +1,18 @@
 import math
 
-def oligotemp(A, C, G, T): 
-	total = A + C + G + T
+def oligotemp(a, c, g, t): 
+	total = a + c + g + t
 	if total <= 13:
-		tm = (A + T) * 2 + (C + G) * 4 
+		tm = (a + t)*2 + (c + g)*4 
 	else:
-		tm = 64.9 + 41* (C + G - 16.4) / total
+		tm = 64.9 + 41*(c + g - 16.4) / total
 	return tm
 
-tm = oligotemp(2, 6, 1, 4)  # (A + C + G + T) = 13
+tm = oligotemp(2, 6, 1, 4)  # (a + c + g + t) = 13
 print(tm)                   # 40
 
-tm = oligotemp(5, 1, 3, 2)  # (A + C + G + T) < 13
+tm = oligotemp(5, 1, 3, 2)  # (a + c + g + t) < 13
 print(tm)                   # 30
 
-tm = oligotemp(8, 4, 7, 6)  # (A + C + G + T) > 13
+tm = oligotemp(8, 4, 7, 6)  # (a + c + g + t) > 13
 print(tm)                   # 56.04400000000001
