b4d0d56a28d7c906b58c410d855e264c8c526946
fragasperini <francescacorinnagasperini@gmail.com>
2024-01-29 21:49:07 -0800

    20demo.py

--- 21quadratic.py
+++ 21quadratic.py
@@ -5,7 +5,7 @@ import sys
 def quadratic(a, b, c): 
 	delta = b**2 - 4*a*c
 	
-	if delta < 0: 
+	if delta < 0:
 		sys.exit('error: delta must be greater than or equal to 0')
 	
 	x1 = (-b + math.sqrt(delta)) / (2*a)
@@ -14,10 +14,10 @@ def quadratic(a, b, c):
 	return x1, x2
 	
 x1, x2 = quadratic(3, 5, 2)
-print (x1, x2)                    # x1 = -0.66666666666666 and x2 = -1
+print(x1, x2)                # x1 = -0.66666666666666 and x2 = -1
 
 x1, x2 = quadratic(1, -2, 1)
-print (x1, x2)                    # x1 = x2 = 1
+print(x1, x2)                # x1 = x2 = 1
 
 x1, x2 = quadratic(3, 4, 2)
-print (x1, x2)                    # error: delta must be greater than or equal to 0
+print(x1, x2)                # error: delta must be greater than or equal to 0
