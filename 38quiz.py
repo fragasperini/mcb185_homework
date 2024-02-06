f2c9672c9c42c2006ae22b5458d1c672fa6952c1
fragasperini <francescacorinnagasperini@gmail.com>
2024-02-06 09:30:45 -0800

    38quiz.py

--- 38quiz.py
+++ 38quiz.py
@@ -1,2 +1,20 @@
 # 38quiz by Francesca C. Gasperini, co-authors isha suba and Amina Muhic
 
+# Gregory-Leibniz
+
+for i in range(1, 8, 2): 
+	d = 3 
+	d = i + 2 
+	print(d)
+
+pi = 1
+for i in range(1, ):
+	if sign == '-': return -i
+	else:           return +i 
+
+	print(sign)
+
+	pi = 4 * (1 + 1 / d)
+	
+limit = 10
+print(pi)
