b8cbda6cbf7d0ac1c884be6c645c19ff4da55883
fragasperini <francescacorinnagasperini@gmail.com>
2024-02-12 23:00:25 -0800

    47deathsaves.py

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -14,19 +14,22 @@ for i in range(trials):
 	failure = 0
 	success = 0
 	
-	for j in range(3):
+	for j in range(5):
 		death_saves = roll()
 		if death_saves >= 10:   success += 1
 		elif death_saves == 1:  failure += 2
-		elif death_saves == 20: success += 1  # revieved
+		elif death_saves == 20: revived += 1  # revieved
 		else:                   failure += 1
 
 	if failure >= 3:   death += 1
 	elif success >= 3: stabilized += 1
-	elif success == 1: revived += 1
+	elif revived > 0:  revived
 
 prob_death = death / trials
 prob_stabilized = stabilized / trials
 prob_revived = revived / trials
 
 print(prob_death, prob_stabilized, prob_revived)
+
+
+# might need to make 5 rolls (but why?)
