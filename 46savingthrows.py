5f8cfa333ecca5de852719432cca4e77161f5f8a
fragasperini <francescacorinnagasperini@gmail.com>
2024-02-13 20:24:49 -0800

    46savingthrows.py

--- 46savingthrows.py
+++ 46savingthrows.py
@@ -5,44 +5,25 @@ import random
 def roll_normal():
 	return random.randint(1, 20)
 
-# Normal
-
-trials = 1000
-print('dc', 'norm', 'adv', 'dis', sep='\t')
-for dc in range(5, 16, 5):
-	n = 0
-	a = 0
-	d = 0
-	print(dc, end= '\t')
-	success = 0 
-	for i in range(trials):
-		rn = roll_normal()
-		if rn > dc: success += 1
-	print(success / trials)
-
-# Advantage
-
 def roll_advantage():
 	roll1 = roll_normal()
 	roll2 = roll_normal() 
 	if roll1 > roll2: return roll1
 	else:             return roll2
 
-	for i in range(trials):
-		rn = roll_advantage()
-		if rn > dc: success += 1
-	print(success / trials)
-
-# Disadvantage
-
 def roll_disadvantage():
 	roll1 = roll_normal()
 	roll2 = roll_normal()
 	if roll1 < roll2: return roll1
 	else:             return roll2
-	for i in range(trials):
-		rn = roll_disadvantage()
-		if rn > dc: success += 1
-	print(success / trials)
-	print(f'{dc}\t{n/trials:.3f}\t{a/trials:.3f}\t{d/trials:.3f}')
 
+trials = 1000
+for dc in range(5, 16, 5):
+	n = 0
+	a = 0
+	d = 0
+	for i in range(trials):
+		if roll_normal() >= dc: n += 1		 # Normal
+		if roll_advantage() >= dc: a += 1	 # Advantage
+		if roll_disadvantage() >= dc: d += 1 # Disadvantage
+	print(f'{dc}\t{n/trials:.3f}\t{a/trials:.3f}\t{d/trials:.3f}')
