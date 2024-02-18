da8b0c3806592f6abaa928d3c0c944aecbafb15e
fragasperini <francescacorinnagasperini@gmail.com>
2024-02-18 11:51:00 -0800

    51cdslength.py

--- 51cdslength.py
+++ 51cdslength.py
@@ -39,9 +39,9 @@ with gzip.open(path, 'rt') as fp:
 		if line[0] != '#':
 			words = line.split()
 			if words[2] == 'CDS':
-				 beg = int(words[3])
-				 end = int(words[4])
-				 lengths.append(end - beg + 1)
+				beg = int(words[3])
+				end = int(words[4])
+				lengths.append(end - beg + 1)
 for length in lengths:
 	print(length)
 #'''
