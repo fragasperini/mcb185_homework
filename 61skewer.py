0b082dfaa30f0cbc2edbd2d467abd7233ab982d1
fragasperini <francescacorinnagasperini@gmail.com>
2024-03-03 17:10:26 -0800

    61skewer.py

--- 61skewer.py
+++ 61skewer.py
@@ -6,4 +6,5 @@ seq = 'ACGTACGGGGGACGTACGTCCCCC'  # make small enough that can be done by hand
 w = 10
 for i in range(len(seq) -w +1):
 	s = seq[i:i+w]
+	#print(i, dogma.gc_comp(s), dogma.gc_skew(s))
 	print(f'{i}\t {dogma.gc_comp(s):.3f}\t{dogma.gc_skew(s):.3f}')
