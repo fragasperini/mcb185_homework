85a7404df352655f7de1afcdd3c223620d57b032
fragasperini <francescacorinnagasperini@gmail.com>
2024-03-11 23:11:48 -0700

    72kmercount.py

--- 72kmercount.py
+++ 72kmercount.py
@@ -13,10 +13,9 @@ for defline, seq in mcb185.read_fasta(sys.argv[1]):
 		kcount[kmer] += 1
 for kmer, n in kcount.items(): print(kmer, n)
 
-# unsure of output with | wc
 
 '''
-Once we get to k = 7  there is one k-mer missing:
+Once we get to k = 7 there is one k-mer missing:
 To find it do the following:
 '''
 
@@ -25,4 +24,8 @@ for nts in itertools.product('ACGT', repeat=k):
 	kmer = ''.join(nts)
 	if kmer in kcount: print(kmer, kcount[kmer])
 	else:              print(kmer, 0)
-# output strano, controllare il file letto
+
+'''
+	#python3 72kmercount.py ecoli.fa.gz 7 | sort -nk2 | head
+	k-mer 'GCCTAGG' is missing, it does not exist in E. coli on the positive strand. 
+'''
