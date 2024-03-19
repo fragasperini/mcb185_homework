cdba50dd05f3ed92f68346b6e6db75e83a5038d6
fragasperini <francescacorinnagasperini@gmail.com>
2024-03-19 12:41:34 -0700

    dogma.py

--- dogma.py
+++ dogma.py
@@ -146,3 +146,29 @@ def entropy(a, c, g, t):
 	
 	h = ha + hc + hg + ht
 	return h
+	
+# Kyte-Doolittle hydrophobicity
+def hydropathy(aa):
+
+	if   aa == 'a': result =  1.80
+	elif aa == "c": result =  2.50
+	elif aa == "d": result = -3.50
+	elif aa == "e": result = -3.50
+	elif aa == "f": result =  2.80
+	elif aa == "g": result = -0.40
+	elif aa == "h": result = -3.20
+	elif aa == "i": result =  4.50
+	elif aa == "k": result =  3.80
+	elif aa == "l": result =  3.80
+	elif aa == "m": result =  1.90
+	elif aa == "n": result = -3.50
+	elif aa == "p": result = -1.60
+	elif aa == "q": result = -3.50
+	elif aa == "r": result = -4.50
+	elif aa == "s": result = -0.80
+	elif aa == "t": result = -0.70
+	elif aa == "v": result =  4.20
+	elif aa == "w": result = -0.90
+	elif aa == "y": result = -1.30
+	else          : result = 'Amino acid unknown'
+	return result
